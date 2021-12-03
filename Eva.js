bd36f031c4d1f765a0ea601009772855db8f6d00
KaranP3 <karanpillai93@gmail.com>
2021-12-03 08:10:59 +0530

    add self-evaluating expressions

---
+++ Eva.js
@@ -0,0 +1,42 @@
+import assert from "assert";
+
+/**
+ * Eva interpreter
+ */
+class Eva {
+  eval(exp) {
+    if (isNumber(exp)) {
+      return exp;
+    }
+
+    if (isString(exp)) {
+      return exp.slice(1, -1);
+    }
+
+    if (exp[0] === "+") {
+      return exp[1] + exp[2];
+    }
+
+    throw "Unimplemented";
+  }
+}
+
+function isNumber(exp) {
+  return typeof exp === "number";
+}
+
+function isString(exp) {
+  return typeof exp === "string" && exp[0] === '"' && exp.slice(-1) === '"';
+}
+
+// -------------------------------
+// Tests
+const eva = new Eva();
+
+assert.strictEqual(eva.eval(1), 1);
+assert.strictEqual(eva.eval('"hello"'), "hello");
+
+assert.strictEqual(eva.eval(["+", 1, 5]), 6);
+assert.strictEqual(eva.eval(["+", ["+", 3, 2], 5]), 10);
+
+console.log("All assertions passed");
